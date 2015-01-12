ea72bec8ffcb0d7c03b7e5bacd86d26550d0f776
Aidan Hobson Sayers <aidanhs@cantab.net>
2015-01-12 22:33:38 +0000

    Use docopt for arguments

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
+#![feature(plugin)]
+
+extern crate "rustc-serialize" as rustc_serialize;
+extern crate docopt;
+#[plugin] #[no_link] extern crate docopt_macros;
 extern crate tclscan;
-use std::os;
+
+use std::io;
+use std::string;
+use docopt::Docopt;
 use tclscan::rstcl;
 
-static HELP: &'static str =
-r"tclscan file <path>
-tclscan str <string>
-tclscan parsestr <string>";
+docopt!(Args derive Show, "
+Usage: tclscan check ( - | <path> )
+       tclscan parsestr ( - | <script-str> )
+");
 
 pub fn main() {
-    let args = os::args();
-    match args.as_slice() {
-        [_, ref op, ref arg] if *op == "file" => {
-            tclscan::scan_file(arg.as_slice());
-        },
-        [_, ref op, ref arg] if *op == "str" => {
-            tclscan::scan_script(arg.as_slice());
-        },
-        [_, ref op, ref arg] if *op == "parsestr" => {
-            println!("{:?}", rstcl::parse_command(arg.as_slice()));
-        },
-        _ => println!("{}", HELP)
+    let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
+    let take_stdin = args.cmd__;
+    let stdin = match take_stdin {
+        true => io::stdin().read_to_string().unwrap(),
+        false => string::String::new(),
     };
+    match (args.cmd_check, args.cmd_parsestr, take_stdin) {
+        (true, false, false) =>
+            tclscan::scan_file(args.arg_path.as_slice()),
+        (true, false, true) =>
+            tclscan::scan_script(stdin.as_slice()),
+        (false, true, false) =>
+            println!("{:?}", rstcl::parse_command(args.arg_script_str.as_slice())),
+        (false, true, true) =>
+            println!("{:?}", rstcl::parse_command(stdin.as_slice())),
+        _ =>
+            panic!("Internal error, cannot handle args"),
+    }
 }
